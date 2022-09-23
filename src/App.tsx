113099ac77823b779c9ce5f26ce22ebd229bf3dc
Patrik Persson <Patrik.2.Persson@sigma.se>
2022-09-23 09:07:13 +0200

    Upload project

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import IssueTable from './pages/Issue-table';
+
+function App() {
+  return (
+    <IssueTable />
+  );
+}
+
+export default App;
---
+++ src/pages/Issue-table.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const IssueTable: React.FC = (props) => {
+
+    return (
+        <div>This is a Issue table</div>
+    )
+}
+
+export default IssueTable
